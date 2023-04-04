03b28a1fe2dad94eaaef4dbbdedec36ed5053d88
Denjiestt <-v>
2023-04-04 11:20:25 +0700

    test54321

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,8 @@ import {Sequelize} from "sequelize"
 const db = new Sequelize('persero','root','',{
     host : '127.0.0.1',
     dialect : 'mysql',
-    port:"3307",
+    port: "3307",
+    connectionLimit: 10,
     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
 
